99916a741619d88d75ba1536681e7b475003d9f6
Murry Ivanoff <murryivanoff@gmail.com>
2015-01-17 18:59:12 +0200

    try select on click

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function displayStops(filter){
 	$(document).ready(function(){
 		stops_html = '';
 		$.each(sofia_stops, function(i, stop){
-			var stop_html = '<a href="#" class="list-group-item spirka-item" style="background-image: url(\'http://www.bgmaps.com/templates/skgt/img/'+stop.id+'\');"><div class="stop-color-overlay"><span class="badge">'+stop.id+'</span><h4 class="list-group-item-heading">'+stop.name+'</h4><p class="list-group-item-text">'+stop.description+' - '+stop.buses+'</p></div></a>';
+			var stop_html = '<a href="#" class="list-group-item spirka-item" style="background-image: url(\'http://www.bgmaps.com/templates/skgt/img/'+stop.id+'\');"><div class="stop-color-overlay"><span class="badge" id="badge-'+stop.id+'">'+stop.id+'</span><h4 class="list-group-item-heading">'+stop.name+'</h4><p class="list-group-item-text">'+stop.description+' - '+stop.buses+'</p></div></a>';
 			stops_html =  stops_html + stop_html;
 		});
 		$('.bus-stops-list').html(stops_html);
@@ -27,7 +27,26 @@ $(document).ready(function(){
 	$( document ).on( "click", "a.spirka-item", function(e) {
 	  e.preventDefault();
 	  //window.clipboardData.setData ("Text", 'tralala');
-	  alert($(this).find('.badge').text()); 
+	  stop_id = $(this).find('.badge').text();
+	  SelectText('badge-' + stop_id);
 	});
 	//window.clipboardData.setData ("Text", input.value);
 });
+
+function SelectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();        
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
